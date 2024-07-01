e8ce7f13120d4ca031dac07bc25ad2530c995df5
Swayam Rajput <swayamrajput029@gmail.com>
2024-07-01 21:11:29 +0530

    fixed a minor fucking mistake

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'/',
+  base:'/react-food-delivery-simulator/',
   plugins: [react()],
   server:{
     port:2000,
